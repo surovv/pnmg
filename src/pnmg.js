dbf3b7adc0cd56ff704f073269251714884fede0
surov <brainsurov@gmail.com>
2017-12-02 15:14:34 +0200

    Add matching for Objects patterns

--- src/pnmg.js
+++ src/pnmg.js
@@ -1,11 +1,12 @@
 /// Types
+
 const someType = class Some{};
 export const Some = new someType;
 // isSome :: a -> Bool
 const isSome = x => x !== undefined;
 
 const condType = class Cond{};
-// Cond :: Function -> a -> Bool
+// Cond :: (a -> Bool) -> a -> Bool
 export const Cond = f => Object.assign(arg => f(arg), {constructor: condType});
 
 /// end Types
@@ -18,13 +19,30 @@ const matchArray = (patterns, vals) => (patterns.length === 0 && vals.length ===
   && patterns.every((pattern, index) => isMatched(pattern, vals[index]))
 );
 
+
+// getObjectsKeys :: [Object] -> [String]
+const getObjectsKeys = objects => objects.map(Object.keys);
+
+// matchObject :: (Object, Object) -> Bool
+const matchObject = (pattern, val) => {
+  const [patternKeys, valKeys] = getObjectsKeys([pattern, val]);
+
+  return (patternKeys.length === 0 && valKeys.length === 0) || (
+    (patternKeys.length <= valKeys.length && patternKeys.length !== 0)
+    && patternKeys.every(k => valKeys.includes(k) && isMatched(pattern[k], val[k]))
+  );
+};
+
+
 // isMatched :: (a, b) -> Bool
 const isMatched = (pattern, val) => (pattern === val)
   || (pattern === val.constructor)
   || (pattern.constructor === someType && isSome(val))
   || (pattern.constructor === condType && pattern(val))
   || (pattern.constructor === Array && matchArray(pattern, val))
+  || (pattern.constructor === Object && matchObject(pattern, val))
 ;
+
 
 
 // match :: (a, [[b, a -> c]]) -> Object
